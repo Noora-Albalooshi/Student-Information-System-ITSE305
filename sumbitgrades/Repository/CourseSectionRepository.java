7b6ca2fc46cb725e6a58bc3e3ee3afdd1566d494
maryam128x <maryam128x@icloud.com>
2026-03-10 23:21:15 +0300

    delet

--- sumbitgrades/Repository/CourseSectionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uob.sis.repository;
-
-import com.uob.sis.entity.CourseSection;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface CourseSectionRepository extends JpaRepository<CourseSection, Long> {
-    Optional<CourseSection> findByIdAndFacultyId(Long courseSectionId, Long facultyId);
-}
--- sumbitgrades/Repository/GradeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uob.sis.repository;
-
-import com.uob.sis.entity.Grade;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface GradeRepository extends JpaRepository<Grade, Long> {
-    Optional<Grade> findByEnrollmentIdAndIsFinalTrue(Long enrollmentId);
-}
