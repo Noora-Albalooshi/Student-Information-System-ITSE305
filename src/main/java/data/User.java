7bfc7e3d253779e231a109f782d61134efe5b217
Noora-Albalooshi <202305890@stu.uob.edu.bh>
2026-04-23 16:00:47 +0300

    Add email and major fields to User class
    Fixed missing variable names for email and major in User class.

--- src/main/java/data/User.java
+++ src/main/java/data/User.java
@@ -4,17 +4,17 @@ package data;
  * Represents a user with email, password, role, full name, and extra info.
  */
 public class User {
-    private String ;
+    private String email;
     private String password;
     private String role; // Student, Faculty, or Admin
     private String fullName;
-    private String major; // for students
+    private String ; // for students
     private double gpa; // for students
     private String department; // for faculty
 
     // Student constructor
     public User(String email , String password, String role, String fullName, String major, double gpa) {
-        this.email = email;
+        this.email = ;
         this.password = password;
         this.role = role;
         this.fullName = fullName;
