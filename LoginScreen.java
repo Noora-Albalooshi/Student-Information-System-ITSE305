e86d27650fecbfbafe3bf5409986326f79b0d04b
Noora-Albalooshi <202305890@stu.uob.edu.bh>
2026-04-23 16:38:06 +0300

    Add files via upload

---
+++ LoginScreen.java
@@ -0,0 +1,54 @@
+package Presentation;
+
+import java.util.Scanner;
+import data.*;
+import business.*;
+
+/*
+ * Presentation Layer
+ * ------------------
+ * Handles user input/output for login.
+ */
+public class LoginScreen {
+    public static void main(String[] args) {
+        UserRepository repo = new UserRepository();
+        LoginService service = new LoginService(repo);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("====================");
+        System.out.println("=== Login System ===");
+        System.out.println("====================");
+        System.out.print("Enter email: ");
+        String email = scanner.nextLine();
+
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+
+        User user = service.authenticate(email, password);
+        System.out.println("=====================");
+        if (user != null) {
+            System.out.println("Login successful! Welcome " + user.getFullName());
+            // Show extra info depending on role
+            if (user.getRole().equals("Student")) {
+                System.out.println("Major: " + user.getMajor());
+                System.out.println("GPA: " + user.getGpa());
+                System.out.println("======================");
+                System.out.println("===Select Service===");
+                System.out.println("   Enroll in Course   ");
+            } else if (user.getRole().equals("Faculty")) {
+                System.out.println("Department: " + user.getDepartment());
+                System.out.println("===================");
+                System.out.println("===Select Service===");
+                System.out.println("   Submit Grade     ");
+            } else if (user.getRole().equals("Admin")) {
+                System.out.println("===Select Service===");
+                System.out.println("     Add Course      ");
+            }
+
+        } else {
+            System.out.println("Login failed. Invalid email or password.");
+        }
+
+        scanner.close();
+    }
+}
