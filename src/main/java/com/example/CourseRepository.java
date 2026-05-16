85ae2365be5b8251367ee409d2887d526f8faae2
Fa6imaebrahim <202307826@stu.uob.edu.bh>
2026-05-16 19:49:35 +0300

    Refactor CourseRepository to use HashMap for storage

--- src/main/java/com/example/CourseRepository.java
+++ src/main/java/com/example/CourseRepository.java
@@ -1,23 +1,18 @@
 package com.example;
-
+import java.util.HashMap; //added
 import java.util.ArrayList;
 import java.util.HashMap;
 // Data layer class responsible for storing courses
 public class CourseRepository {
-    private HashMap<String,Course> courses = new HashMap<>();
+    private HashMap<String,Course> courses = new HashMap<>(); //HashMap instaed of ArrayList
 
     // Adds a course to the data storage
     public void addCourse(Course course) {
-        courses.add(course);
+        courses.put(course.getCourseId().toLowerCase(),course);
     }
 
     // Checks if a course ID already exists
     public boolean existsById(String id) {
-        for (Course course : courses) {
-            if (course.getCourseId().equalsIgnoreCase(id)) {
-                return true;
-            }
-        }
-        return false;
+        return courses.containsKey(id.toLowerCase());
     }
 }
